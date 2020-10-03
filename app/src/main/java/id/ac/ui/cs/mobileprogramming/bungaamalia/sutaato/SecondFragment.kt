ac0aad145bf49c3061f2c2190b8b93b16f4e9836
Bunga Amalia <bungasokia@gmail.com>
2020-10-04 02:28:42 +0700

    Implement random number on second fragment

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/sutaato/SecondFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/sutaato/SecondFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,11 +23,24 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    private val args: SecondFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        val value = args.randomArg
+        val valueText = getString(R.string.random_heading, value)
+        view.findViewById<TextView>(R.id.textView_header).text = valueText
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (value > 0) {
+            randomNumber = random.nextInt(value + 1)
+        }
+        view.findViewById<TextView>(R.id.textView_random).text = randomNumber.toString()
     }
 }
