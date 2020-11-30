1f3cdef25ac32eae8d3a23667547fffaa20a77b4
Bunga Amalia <bungasokia@gmail.com>
2020-11-30 20:40:09 +0700

    Implement wi-fi scan

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/sutaato/FirstFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/sutaato/FirstFragment.kt
@@ -1,5 +1,11 @@
 package id.ac.ui.cs.mobileprogramming.bungaamalia.sutaato
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +15,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 
@@ -29,11 +36,44 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val wifiManager = activity?.applicationContext?.getSystemService(
+            Context.WIFI_SERVICE) as WifiManager
+
+        val wifiScanReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val wifiNames = wifiManager.scanResults.map {
+                    it.SSID
+                }
+
+                if (wifiNames.isNotEmpty()) {
+                    view.findViewById<TextView>(R.id.wifi_textView).text =
+                        wifiNames.joinToString(
+                            prefix = "Wi-Fi names: [", postfix = "]", separator = ", ")
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        activity?.applicationContext?.registerReceiver(wifiScanReceiver, intentFilter)
+
+        view.findViewById<Button>(R.id.wifi_button).setOnClickListener {
+            val success = wifiManager.startScan()
+            if (success) {
+                Toast.makeText(activity?.applicationContext,
+                    "Scanning for Wi-Fi, loading...", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(activity?.applicationContext,
+                    "Wi-Fi scanning failed, please try again.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         view.findViewById<Button>(R.id.start_button).setOnClickListener {
             val showValueTextView = view.findViewById<TextView>(R.id.textView_input)
             viewModel.currentValue = showValueTextView.text.toString().toInt()
             if (viewModel.currentValue == 0) {
-                Toast.makeText(activity?.applicationContext, "Countdown can't start from 0!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity?.applicationContext,
+                    "Countdown can't start from 0!", Toast.LENGTH_SHORT).show()
             } else {
                 val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment()
                 findNavController().navigate(action)
